d06fb25937dde00ca8e8f5d5634722d62a5b3dc2
KiseraTimon <timonmarick@gmail.com>
2025-06-13 19:06:15 +0300

    #26 - Secret Key Config & Auth Blueprint Registration

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
+from .config import env
 
 def create_app():
     global app
 
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = "flaskapp"
+    app.config['SECRET_KEY'] = env.SECRET_KEY
 
     # Blueprints
-    from .blocks.pages.pages import pages
+    from .assets.pages.pages import pages
+    from .assets.auth.auth import auth
 
     # Registering Blueprints
     app.register_blueprint(pages, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
