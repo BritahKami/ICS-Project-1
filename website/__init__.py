4e399b90da049a43b767db92c289b1c7f6dc616c
KiseraTimon <timonmarick@gmail.com>
2025-06-18 15:35:57 +0300

    #39 - Mailing Setup & Binding

--- website/__init__.py
+++ website/__init__.py
@@ -1,19 +1,48 @@
 from flask import Flask
-from .config import env
+
+from utils import errhandler, mail as MAIL
+from config import env
 
 def create_app():
     global app
 
+    # Initializing Flask
     app = Flask(__name__)
 
+    # Configuring Secret Key
     app.config['SECRET_KEY'] = env.SECRET_KEY
 
-    # Blueprints
+    # Importing Blueprints
     from .assets.pages.pages import pages
     from .assets.auth.auth import auth
 
     # Registering Blueprints
     app.register_blueprint(pages, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    # app.register_blueprint(mailer, url_prefix='/')
 
+    """
+    Flask Mail
+    """
+    # Mail Configurations
+    app.config.update(
+        MAIL_SERVER=env.MAIL_SERVER,
+        MAIL_PORT=env.MAIL_PORT,
+        MAIL_USERNAME=env.MAIL_USERNAME,
+        MAIL_PASSWORD=env.MAIL_PASSWORD,
+        MAIL_USE_TLS= env.MAIL_USE_TLS,
+        MAIL_USE_SSL=env.MAIL_USE_SSL,
+        # MAIL_DEFAULT_SENDER=env.MAIL_SENDER,
+        # MAIL_SENDER=env.MAIL_SENDER,
+        # MAIL_RECEIVER=env.MAIL_RECEIVER,
+
+        # Defaults
+        MAIL_MAX_EMAILS = None,
+        MAIL_ASCII_ATTACHMENTS = False
+    )
+
+    # Binding Extensions
+    MAIL.init_app(app)
+
+    # Returning App Instance
     return app
