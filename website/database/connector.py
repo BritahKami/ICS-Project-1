0fb18541ba9e722270b43ad14568135a81c60374
KiseraTimon <timonmarick@gmail.com>
2025-06-18 15:35:15 +0300

    #37 - Database Reconfiguration

--- website/database/connector.py
+++ website/database/connector.py
@@ -1,7 +1,7 @@
 import mysql.connector
-from ..config import env
+from config import env
 
-#Establishing SQL Connection
+# Establishing SQL Connection
 def dbconnector():
     return mysql.connector.connect(
         host=env.DB_HOST,
