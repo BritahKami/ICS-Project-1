490a4566ff5e1a1c6f3fcf5aeb3cfdde6fe01e84
BritahKami <mukamibridget7@gmail.com>
2025-07-02 00:31:46 +0300

    #115 - Updated students route

--- website/assets/pages/dashboards.py
+++ website/assets/pages/dashboards.py
@@ -1,11 +1,20 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session, flash
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash, current_app
 from website.database.connector import dbconnector
+from utils import errhandler
+import os 
+from werkzeug.utils import secure_filename
+
 
 # Admin Blueprint Instance
 dash = Blueprint('dash', __name__)
 
 # Database Connection
 conn = dbconnector()
+
+#allowed image extensions
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # Access Verification Route
 @dash.route('/dashboard', methods=['GET', 'POST'])
@@ -100,6 +109,7 @@ def business():
 
     return render_template('dashboard/businesses/business.html')
 
+
 # Student Dashboard Route
 @dash.route('/dashboard/student', methods=['GET', 'POST'])
 def student():
@@ -124,5 +134,48 @@ def student():
 
         # Redirecting
         return redirect(url_for('auth.signin'))
+    
+    if request.method == 'POST':
+        image = request.files.get('image')
+        title = request.form.get('title')
+        description = request.form.get('description')
+
+        # Validating Entries
+        if not (image and title and description):
+            flash('Kindly fill in all fields', category='error')
+
+            return redirect(request.url)
+        
+        if image and allowed_file(image.filename):
+            
+            try:
+                #secure and save the file
+                filename= secure_filename(image.filename)
+                image.save(os.path.join(current_app.config['UPLOAD_FOLDER'],filename))
+
+                # inserting into database
+                cursor = conn.cursor(dictionary=True)
+                cursor.execute("INSERT INTO projects (image, title, description) VALUES (%s, %s, %s)", (filename, title, description))
+                conn.commit()
+
+                flash('Your project has been added successfully', category="success")
+
+                return redirect(url_for('pages.homepage'))
+
+            # Handling Exceptions
+            except Exception as e:
+                errhandler(e, 'pages/project')
+
+                flash('An error has occurred. Try again later', category='error')
+
+            # Closing Cursor
+            finally:
+                if 'cursor' in locals() and cursor is not None:
+                    cursor.close()
+        
+        else:
+            flash("Invalid image format. Allowed types: png, jpg, jpeg, gif.", category='error')
+            return redirect(request.url)
+
 
     return render_template('dashboard/students/student.html')
