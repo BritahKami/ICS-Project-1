df3242f1e8e3ea479ee0e5d78703d65c3ac04f7c
KiseraTimon <timonmarick@gmail.com>
2025-07-03 09:28:26 +0300

    #119 - Backend Logic to Process Adding Jobs

--- website/assets/processes/processes.py
+++ website/assets/processes/processes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash, request, redirect, url_for
+from flask import Blueprint, render_template, flash, request, redirect, url_for, session
 from website.database.connector import dbconnector
 from utils import errhandler
 
@@ -72,3 +72,107 @@ def newsletter():
 
     # Redirecting
     return redirect(url_for("pages.homepage"))
+
+# Add Item
+@process.route('/addItem/', methods=['GET', 'POST'])
+def addItem():
+    # Validating if the User Has Access
+    if not (session['userID']):
+        flash("You are not authorized to access this service", category='error')
+
+        # Clearing Session
+        session.clear()
+
+        # Redirecting
+        return redirect(url_for(auth.signin))
+
+    # Checking Request Method
+    if request.method == 'POST':
+        # Capturing Form Entries
+        title = request.form.get('title')
+        option = request.form.get('option')
+        description = request.form.get('description')
+        icon = request.files['image']
+
+        # Validating Entries
+        if not (title or option or description or icon):
+            # Error Message
+            flash("Please fill in all fields", category="error")
+
+            # Redirecting
+            return redirect(request.url)
+
+        # Image Path Processing
+        from utils import imghandler
+        icon = imghandler(
+            icon,
+            path='uploads',
+            subPath="items",
+            operation='add'
+        )
+
+        print(icon)
+
+        # Operations
+        try:
+            # Initializing Cursor
+            cursor = conn.cursor(dictionary=True)
+
+            # Checking if the User Has a Valid Business Account
+            cursor.execute("SELECT * FROM businesses WHERE userID = %s", (session['userID'],))
+            business = cursor.fetchone()
+
+            # Validating Query Result
+            if business and (business != None):
+                # Capturing Business ID
+                businessID = business['businessID']
+
+                # Verifying UserID Matches Session Data
+                if business['userID'] != session['userID']:
+                    # Error Message
+                    flash("Your credentials are corrupted. You cannot complete this operation", category="error")
+
+                    # Redirecting
+                    return redirect(auth.logout)
+
+                else:
+                    userID = business['userID']
+            else:
+                # Error Message
+                flash("You do not have an active business account", category="error")
+
+                # Redirecting
+                return redirect(auth.logout)
+
+            # Executing Query
+            cursor.execute(f"INSERT INTO {option.lower() + 's'} (title, description, icon, userID, businessID) VALUES (%s, %s, %s, %s, %s)", (title, description, icon, userID, businessID))
+
+            # Committing to Database
+            conn.commit()
+
+            # Success Message
+            flash(f"Your {option} has been successfully added", category="success")
+
+            # Redirecting
+            return redirect(url_for('dash.dashboard'))
+
+        # Handling Errors
+        except Exception as e:
+            # Database Rollback
+            conn.rollback()
+
+            # Logging Error
+            errhandler(e, 'process/add-items')
+
+            # Error Message
+            flash("An error occurred adding your item", category="error")
+
+            # Redirecting
+            return redirect(request.url)
+
+        # Closing Cursor
+        finally:
+            if 'cursor' in locals() and cursor is not None:
+                cursor.close()
+
+    return redirect(url_for('dash.dashboard'))
